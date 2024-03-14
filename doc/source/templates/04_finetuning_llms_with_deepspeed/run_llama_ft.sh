87425c5e4f48366822fa8fd6067e3ca39c8b7d0d
ddl-wadkars <77989508+ddl-wadkars@users.noreply.github.com>
2024-03-14 13:57:07 -0400

    Update run_llama_ft.sh
    1. Use an environment variable to select the dataset
    2. Set default nd=6 for 7B model
    
    Signed-off-by: ddl-wadkars <77989508+ddl-wadkars@users.noreply.github.com>

--- doc/source/templates/04_finetuning_llms_with_deepspeed/run_llama_ft.sh
+++ doc/source/templates/04_finetuning_llms_with_deepspeed/run_llama_ft.sh
@@ -43,7 +43,7 @@ fine_tune() {
 }
 
 # Variables for cleaner handling
-BASE_DIR="/mnt/data/reference-ray-domino-llama2" # your dataset path
+BASE_DIR="/mnt/data/${DOMINO_PROJECT_NAME}" # your dataset path
 DATA_DIR="./data" #"./data"
 TRAIN_PATH="${DATA_DIR}/train.jsonl"
 TEST_PATH="${DATA_DIR}/test.jsonl"
@@ -66,15 +66,15 @@ done
 case $SIZE in
 "7b")
     BS=16
-    ND=16 # can be changed depending on your hardware
+    ND=6 #16 #6 #16 node count 1 with gpu-training-a10 hwt was successful with 7b
     ;;
 "13b")
     BS=16
-    ND=16 # can be changed depending on your hardware
+    ND=16 #16 node count 16 works for 13b with gpu-testing hwt and 16 workers 
     ;;
 "70b")
     BS=8
-    ND=32 # can be changed depending on your hardware
+    ND=32 #16 #32
     ;;
 *)
     echo "Invalid size: ${SIZE}"
